1080faa535ce216749cfa165a90f683b0b1ea0c1
Natchanon Hongladaromp <natchanon.h@hotmail.com>
2014-04-17 21:30:55 +0700

    Dead when hit obstacle

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -14,45 +14,50 @@ var GameLayer = cc.LayerColor.extend({
 
         this.setKeyboardEnabled( true );
         this.scheduleUpdate();
+        this.status = GameLayer.STATUS.START;
         return true;
     },
 
 
     onKeyDown: function( e ) {
-        switch( e ) {
-        case cc.KEY.left:
-            this.fox.setNextDirection( Fox.DIR.LEFT );
-            break;
-        case cc.KEY.right:
-            this.fox.setNextDirection( Fox.DIR.RIGHT );
-            break;
-        case cc.KEY.up:
-            this.fox.setNextDirection( Fox.DIR.UP );
-            break;
-        case cc.KEY.down:
-            this.fox.setNextDirection( Fox.DIR.DOWN );
-            break;
+        if( this.status != GameLayer.STATUS.END ) {
+            switch( e ) {
+            case cc.KEY.left:
+                this.fox.setNextDirection( Fox.DIR.LEFT );
+                break;
+            case cc.KEY.right:
+                this.fox.setNextDirection( Fox.DIR.RIGHT );
+                break;
+            case cc.KEY.up:
+                this.fox.setNextDirection( Fox.DIR.UP );
+                break;
+            case cc.KEY.down:
+                this.fox.setNextDirection( Fox.DIR.DOWN );
+                break;
+            }
         }
     },
 
     onKeyUp: function( e ) {
-        switch( e ) {
-        case cc.KEY.left:
-            if( this.fox.direction == Fox.DIR.LEFT );
-            this.fox.setNextDirection( Fox.DIR.STILL );
-            break;
-        case cc.KEY.right:
-            if( this.fox.direction == Fox.DIR.RIGHT );
-            this.fox.setNextDirection( Fox.DIR.STILL );
-            break;
-        case cc.KEY.up:
-            if( this.fox.direction == Fox.DIR.UP );
-            this.fox.setNextDirection( Fox.DIR.STILL );
-            break;
-        case cc.KEY.down:
-            if( this.fox.direction == Fox.DIR.DOWN );
-            this.fox.setNextDirection( Fox.DIR.STILL );
-            break;
+        if( this.status != GameLayer.STATUS.END ) {
+            switch( e ) {
+            case cc.KEY.left:
+                if( this.fox.direction == Fox.DIR.LEFT );
+                this.fox.setNextDirection( Fox.DIR.STILL );
+                break;
+            case cc.KEY.right:
+                if( this.fox.direction == Fox.DIR.RIGHT );
+                this.fox.setNextDirection( Fox.DIR.STILL );
+                break;
+            case cc.KEY.up:
+                if( this.fox.direction == Fox.DIR.UP );
+                this.fox.setNextDirection( Fox.DIR.STILL );
+                break;
+            case cc.KEY.down:
+                if( this.fox.direction == Fox.DIR.DOWN );
+                this.fox.setNextDirection( Fox.DIR.STILL );
+                break;
+            }
         }
     },
 
@@ -68,12 +73,17 @@ var GameLayer = cc.LayerColor.extend({
         for(var i=0; i<obstacles.length; i++) {
             if( obstacles[i].hit( this.fox ) )
             {
-                console.log("aaa");
+                this.status = GameLayer.STATUS.END;
             }
             
         }
     },
 });
+
+GameLayer.STATUS = {
+    START: 0,
+    END: 1
+}
 
 var StartScene = cc.Scene.extend({
     onEnter: function() {
